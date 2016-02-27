b8f89c409272837521cef42732e182fcf8da240e
richlvnsn <richlvnsn@gmail.com>
2016-02-27 17:33:54 -0500

    Added lists of strings with filtering for keyword and length

--- markovtest.py
+++ markovtest.py
@@ -6,18 +6,17 @@ rubio_file = open("rubio.txt", "r")
 kasich_file = open("kasich.txt", "r")
 carson_file = open("carson.txt", "r")
 
-mcTrump = MarkovChain("./trumpdb")
-mcCruz = MarkovChain("./cruzdb")
-mcRubio = MarkovChain("./rubiodb")
-mcKasich = MarkovChain("./kasichdb")
-mcCarson = MarkovChain("./carsondb")
-
-
 trumpText = trump_file.read()
 cruzText = cruz_file.read()
 rubioText = rubio_file.read()
 kasichText = kasich_file.read()
 carsonText = carson_file.read()
+
+mcTrump = MarkovChain("./trumpdb")
+mcCruz = MarkovChain("./cruzdb")
+mcRubio = MarkovChain("./rubiodb")
+mcKasich = MarkovChain("./kasichdb")
+mcCarson = MarkovChain("./carsondb")
 
 mcTrump.generateDatabase(trumpText)
 mcCruz.generateDatabase(cruzText)
@@ -31,8 +30,29 @@ mcRubio.dumpdb()
 mcKasich.dumpdb()
 mcCarson.dumpdb()
 
-print mcTrump.generateString() + ".\n"
-print mcCruz.generateString() + ".\n"
-print mcRubio.generateString() + ".\n"
-print mcKasich.generateString() + ".\n"
-print mcCarson.generateString() + ".\n"
+trumpList = []
+cruzList = []
+rubioList = []
+kasichList = []
+carsonList = []
+
+for x in xrange(1,100):
+	trumpList.append(mcTrump.generateString())
+	cruzList.append(mcCruz.generateString())
+	rubioList.append(mcRubio.generateString())
+	kasichList.append(mcKasich.generateString())
+	carsonList.append(mcCarson.generateString())
+	# pass
+
+
+keyword = "Mexico".lower()
+done = 0;
+print len(trumpList)
+for x in xrange(0,99):
+	splitted = trumpList[x].split()
+	for y in range(0, len(splitted)):
+		if ((splitted[y].lower() == keyword) and (len(trumpList[x].split()) > 4) and done == 0):
+			print trumpList[x]
+			print len(trumpList[x].split())
+			done = 1
+
