4de33c634e373bac109cf39013671e960c025c7c
hdubel94 <hunterdubel@gmail.com>
2016-02-27 22:07:09 -0500

    Messing with Message View

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import kivy
+import random
 
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.graphics import *
 from kivy.uix.boxlayout import BoxLayout
 
+"""red = [1,0,0,1]
+green = [0,1,0,1]
+blue =  [0,0,1,1]
+purple = [1,0,1,1]
+"""
 class drawChat(Widget):
+	#colors = [red, green, blue, purple]
 	pass
+	
 
 class drawMenu(Widget):
 	pass
 		
 class DebateSimApp(App):
+	#candidates = ["Trump", "Cruz", "Rubio", "Kasich", "Carson", "Bush", "Christie"]
+	speaker = "Bluhaha!"
 	def build(self):
+		self.load_kv('DebateSim.kv')
 		return drawMenu()
 
 if __name__ == "__main__":
