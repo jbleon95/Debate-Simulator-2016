fd5e43c9095c9f6551993d671fcfe5014affbb4f
Jeremy Leon <jeremybenleon@gmail.com>
2016-02-27 22:32:52 -0500

    Cleaned up databases and transcripts, added more keywords

--- questions.py
+++ questions.py
@@ -1,44 +1,64 @@
 questions = {
 	0: [", with the death of Justice Scalia, what kind of justice would you nominate?",
-	["supreme", "court", "scalia", "justice", "judicial"]],
+	["supreme", "court", "scalia", "justice", "judicial", "Roberts", "judge"]],
+
 	1: [", it's your first day in the situation room. What three questions do you ask your national security experts about the world?",
-	["security","middle", "sunni", "shia", "islam", "syria", "isis", "iraq"]],
+	["security","middle", "sunni", "shia", "islam", "syria", "isis", "iraq", "russia", "china", "korea", "putin", "jong-un", "cuba", "castro"]],
+
 	2: [", ISIS and the fighting in the Middle-East have been the center of foreign policy discussion. What would your foreign policy look like?",
-	["security","middle", "sunni", "shia", "islam", "syria", "isis", "iraq"]],
+	["security","middle", "sunni", "shia", "islam", "syria", "isis", "iraq", "iran", "israel", "palestine", "israeli"]],
+
 	3: [", should financial executives be held legally responsible for financial crisis, and do you think fines like these are an effective way to deter companies from future behavior like that?",
-	["finance", "wall", "tax", "taxes", "budget"]],
+	["finance", "street", "tax", "taxes", "budget", "wealthy", "flat", "wage", "executives", "business", "loan", "loans"]],
+
 	4: [", how would you cut $10 trillion over 10 years, but make sure the country isn't saddled with even more debt?",
-	["finance", "wall", "tax", "taxes", "budget", "debt"]],
+	["finance", "street", "tax", "taxes", "budget", "wealthy", "flat", "wage", "business", "loan", "loans", "debt", "trillion"]],
+
 	5: [", the American people are divided on the subject of immigration. How would you approach amnesty?",
-	["immigration", "immigrants", "amnesty", "mexico", "wall"]],
+	["immigration", "immigrants", "amnesty", "mexico", "wall", "border", "anchor", "visa", "visas", "mexican"]],
+
 	6: [", the subject of a wall on the Mexico border is gaining support, would you support it, and who would pay?",
-	["immigration", "immigrants", "amnesty", "mexico", "wall"]],
-	7: [", most Americans are now in favor of medical marijuana? Is this up to the federal government, or should the states decide?",
+	["immigration", "immigrants", "amnesty", "mexico", "wall", "border", "anchor", "visa", "visas", "mexican"]],
+
+	7: [", most Americans are now in favor of medical marijuana. Is this up to the federal government, or should the states decide?",
 	["medical", "marijuana", "weed", "cannabis"]],
-	8: [", Planned Parenthood recently denied the allegations of the videos released recently. Would you continue to fund them?",
-	["planned", "parenthood", "abortion", "fetus"]],
-	9: [", America is divided on the issue of gun control? How do you plan on protecting our Second Amendment?",
-	["gun", "amendment"]],
+
+	8: [", Planned Parenthood recently denied the allegations of the videos released recently. Would you continue to fund them in your administration?",
+	["planned", "parenthood", "abortion", "fetus", "pro", "pro-life", "pro-choice", "womb", "pregnant", "pregnancy", "concieve", "concieved", "conception"]],
+
+	9: [", America is divided on the issue of gun control. How do you plan on protecting our Second Amendment?",
+	["gun", "amendment", "second", "rifle", "pistol", "hunters", "hunting"]],
+
 	10: [", how do you plan on protecting Religious Liberty?",
-	["religious", "god", "church"]],
-	11: [", what role does G-d play in your campaign?",
-	["religious", "god", "church"]],
+	["religious", "god", "church", "marriage", "baker", "cake", "wedding", "birth", "contraceptives"]],
+
+	11: [", what role does God play in your campaign?",
+	["religious", "god", "church", "pope", "religion", "holy", "priest"]],
+
 	12: [", which of Obama's executive actions would you repeal your first day?",
 	["barack", "obama", "executive"]],
+
 	13: [", many Americans are in support of parts of Obamacare. How would your plan replace Obamacare?",
-	["obamacare", "health", "insurance", "repeal"]],
+	["obamacare", "health", "insurance", "repeal", "hospital", "patient", "medicare", "medicaid"]],
+
 	14: [", many Americans worry that they won't have access to Social Security. How would you manage the growing amount of retirees?",
-	["social", "retire", "retirees"]],
+	["social", "retire", "retirees", "medicare", "welfare"]],
+
 	15: [", how are you going to bring jobs back to America?",
 	["job", "jobs"]],
+
 	16: [", how are you going to deal with our decaying infrastructure.",
-	["bridge", "bridges", "infrastructure"]],
+	["bridge", "bridges", "infrastructure", "road", "roads", "construction", "rail", "trains"]],
+
 	17: [", how would you campaign against Bernie Sanders or Hillary Clinton?",
 	["bernie", "sanders", "hillary", "clinton"]],
+
 	18: [", with the current instabiltiy of the Middle-East, how would you support our Israeli allies?",
-	["security","middle", "sunni", "shia", "islam", "syria", "isis", "iraq", "israel", "israeli"]],
+	["security","middle", "sunni", "shia", "islam", "syria", "isis", "iraq", "iran", "israel", "palestine", "israeli"]],
+
 	19: [", how does Ronald Reagan influence you?",
 	["ronald", "reagan"]],
+
 	20: [", who is your favorite president, and why is it Ronald Reagan?",
 	["ronald", "reagan"]]
 }
